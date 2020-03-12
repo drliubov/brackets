2c877c8a7b4f0d410b94f96981168ebb96f5d96f
drliubov <druzyaka.liubov@gmail.com>
2020-03-12 13:09:28 +0300

    feat: add solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let bracket = bracketsConfig.map( (b) => b.join("") );
+
+  for (let i = 0; i < bracket.length; i++) {
+    if( str.includes(bracket[i]) ) {
+      str = str.replace(bracket[i], "");
+      i = -1;
+    }
+    
+  }
+  return (str) ? false : true;
 }
